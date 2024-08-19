7d652f51258e09e2714696799a61b3fc9c35f910
Mariam <mariam.yasser772000@gmail.com>
2024-08-19 12:05:11 +0000

    done

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,6 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Return a tuple containing a start index and an end index corresponding to the range of indexes"""
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+    """Retrieves the index range from a given page and page size"""
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
