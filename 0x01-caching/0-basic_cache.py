0e3edebda069c4fad133773a11ae9e24807f146a
Mariam <mariam.yasser772000@gmail.com>
2024-08-22 20:10:10 +0000

    done

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,10 +7,10 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """BasicCache that inherits from BaseCaching and is a caching system"""
+    """A class `BasicCache` that inherits from `BaseCaching`"""
 
     def put(self, key, item):
-        """assign to the dictionary self.cache_data the item value for the key"""
+        """assign to the `self.cache_data` the item value for the key"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
