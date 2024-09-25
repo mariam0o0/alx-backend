bb37ba2a67b97876bff347f71e8320e00e96f139
Mariam <mariam.yasser772000@gmail.com>
2024-09-25 19:03:11 +0000

    0x03

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,13 @@
+import { createQueue } from 'kue';
+
+
+const queue = createQueue();
+
+const jobData = { phoneNumber: '01082252819', message: 'Hello!' };
+
+const job = queue.create('push_notification_code', jobData).save((error) => {
+  if (!error) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
